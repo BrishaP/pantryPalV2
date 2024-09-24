2921fe5f9497516fd2577d0489fad486e3749bc5
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-09-24 22:03:35 +0100

    help

--- byte-sized-snacks/src/components/Main/Main.jsx
+++ byte-sized-snacks/src/components/Main/Main.jsx
@@ -2,14 +2,12 @@
 
 import React from 'react';
 import AddItem from '../AddItem/AddItem';
-import { Button } from "@/components/ui/button"
+
 
 const Main = () => {
   return (
     <main>
-    <div>
-      <Button>Click me</Button>
-    </div>
+   
         <AddItem />
     </main>
   );
