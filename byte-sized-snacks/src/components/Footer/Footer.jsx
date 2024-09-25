4e274f1437fedfaff48e22b7d9dcb1f6e5d54e04
Richard Lamb <lamby42@live.co.uk>
2024-09-25 17:30:22 +0100

    fixed <a> tag issue in footer causing hydration error - will also merge and test deployment again

--- byte-sized-snacks/src/components/Footer/Footer.jsx
+++ byte-sized-snacks/src/components/Footer/Footer.jsx
@@ -1,48 +1,40 @@
 "use client";
 
-import React from 'react';
-import Link from 'next/link';
-import styles from './Footer.module.css';
-import Image from "next/image"
-
-
+import React from "react";
+import Link from "next/link";
+import styles from "./Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
-  const pageLinks = [
-     
-   { href: '/item', text: '', image: '/images/fridge.png' },
- 
-    { href: '/shopping', text: '', image: '/images/add.png' },
-   
-      
-  ];
+    const pageLinks = [
+        { href: "/item", text: "", image: "/images/fridge.png" },
 
+        { href: "/shopping", text: "", image: "/images/add.png" },
+    ];
 
-  return (
-    <footer className={styles.footer}>
-      <ul className= {styles.footerList}>
-        {pageLinks.map((link, index) => (
-          <li key={index} className= {styles.footerItem}>
-            <Link href={link.href}>
-              <a className= {styles.footerLink}>
-                {link.image &&
-                  <Image
-                    src={link.image}
-                    alt={link.name}
-                    width={70}
-                    height={70}
-                    className="footerIcon"
-                  />}
-                
-                  
-               </a>
-              {link.icon}</Link>
-           
-          </li>
-        ))}
-      </ul>
-    </footer>
-  );
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.footerList}>
+                {pageLinks.map((link, index) => (
+                    <li key={index} className={styles.footerItem}>
+                        <Link href={link.href} className={styles.footerLink}>
+                            {link.image && (
+                                <Image
+                                    src={link.image}
+                                    alt={link.name}
+                                    width={70}
+                                    height={70}
+                                    className="footerIcon"
+                                />
+                            )}
+
+                            {link.icon}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </footer>
+    );
 };
 
 export default Footer;
